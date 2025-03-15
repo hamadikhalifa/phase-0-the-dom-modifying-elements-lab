fdbf88f65f91ab31834e3782c3d3076b0db933dd
hamadikhalifa <ekidorevans719@gmail.com>
2025-03-16 01:15:51 +0300

    complete DOM manipulation lab: remove main add newHeader

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"; 
+
+
+newHeader.textContent = "YOUR-NAME is the champion"; // Replace YOUR-NAME
+
+
+document.body.append(newHeader);
